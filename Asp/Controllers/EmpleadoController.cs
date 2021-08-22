ed664f6a3831d234815f94c32e4c68c1b18155e8
Luis Serrano <84140881+LuisBy860@users.noreply.github.com>
2021-08-22 15:52:32 -0600

    asp actualizado

--- Asp/Controllers/EmpleadoController.cs
+++ Asp/Controllers/EmpleadoController.cs
@@ -10,7 +10,7 @@ namespace Asp.Controllers
     public class EmpleadoController : Controller
     {
         // GET: Empleado
-        public ActionResult Empleado()
+        public ActionResult Empleado() //este es el controller
         {
 
             using (EmpleadoEntities bd = new EmpleadoEntities())
@@ -21,6 +21,42 @@ namespace Asp.Controllers
                 return View(ListadoEmpleado);
             }
         }
-    }
+        public ActionResult save(string nombre, string direccion)
+        {
+            using (EmpleadoEntities bd = new EmpleadoEntities())
+            {
+                Empleado emp = new Empleado();
+                emp.Nombre = nombre;
+                emp.Direccion = direccion;
+                bd.Empleado.Add(emp); //este es como la funcion agregar de un crud 
+                bd.SaveChanges();//con esto la base de datos reconoce los datos ingresados 
+
+                return Redirect("/Empleado/Empleado"); //si hago un rediccionamiento  cuando colocamos un viewbag no lo va cargar
+            }
+
+
+
+        }
+        public ActionResult Registro()
+        {
+            string nombre = "Luis";
+            ViewBag.EnviandoDatosRegistro = nombre;
+            return View();
+           
+          
+        }
+        public ActionResult Delete(int id)
+        {
+
+            using (EmpleadoEntities bd = new EmpleadoEntities())
+            {
+                Empleado eliminardatos = bd.Empleado.Where(x => x.IdEmpleado == id).FirstOrDefault(); //EF,expreciones lamba y linq(lenguaje de consulta a colecciones de datos )  
+                bd.Empleado.Remove(eliminardatos); //elimina el dato y guardamos con savechanges
+                bd.SaveChanges();  
+            return Redirect("/Empleado/Empleado");
+            }
+        }
+    } 
 }
+
     
